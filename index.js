4ea41d91361992c6bb49ca740e0b771a34e7d64a
PabloPaez <pablopaezguerrero07@gmail.com>
2023-06-02 21:17:24 +0200

    no message

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const mqtt = require('mqtt');
+const GPIO = require('rpi-gpio');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+GPIO.setMode('mode_bcm');
+GPIO.setup(17,'out');
+GPIO.setup(27,'out');
+GPIO.setup(22,'out');
 
 app.get('/', (req, res) => {
     res.render('pagina_principal.ejs', {
@@ -23,11 +29,66 @@ app.get('/aplicacion', (req, res) => {
 })
 
 app.get('/contacto', (req, res) => {
+    GPIO.output(17,false);
+    GPIO.output(22,false);
+    GPIO.output(27,false);
     res.render('contacto.ejs', {
         root:__dirname
     })
 })
 
-app.listen(3000);
+app.get('/aplicacion/luces/:num_boton', (req, res) => {
+    var client = mqtt.connect('tcp://test.mosquitto.org');
+    switch(req.params.num_boton)
+    {
+        case "1":
+            console.log('enciendo luz 1');
+            client.on('connect', function()
+            {
+                client.subscribe('topic_luces', function (error)
+                {
+                    if(!error)
+                    {
+                        client.publish('topic_luces', 'Enciendo habitacion 1')
+                    }
+                })
+            })
+            GPIO.output(17,true);
+        break;
+        case "2":
+            console.log('enciendo luz 2');
+            client.on('connect', function()
+            {
+                client.subscribe('topic_luces', function (error)
+                {
+                    if(!error)
+                    {
+                        client.publish('topic_luces', 'Enciendo habitacion 2')
+                    }
+                })
+            })
+            GPIO.output(27,true);
+        break;
+        case "3":
+            console.log('enciendo luz 3');
+            client.on('connect', function()
+            {
+                client.subscribe('topic_luces', function (error)
+                {
+                    if(!error)
+                    {
+                        client.publish('topic_luces', 'Enciendo habitacion 3')
+                    }
+                })
+            })
+            GPIO.output(22,true);
+        break;
+        default:
+            return 0;
+        break;
+    }
+})
+
+app.listen(2000);
 console.log('Server on port ${3000}');
 
