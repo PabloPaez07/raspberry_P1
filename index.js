8603c0702da8bb31119a69c562d4026dad01ff40
PabloPaez <pablopaezguerrero07@gmail.com>
2023-06-21 18:39:20 +0200

    no message

--- index.js
+++ index.js
@@ -99,7 +99,24 @@ app.get('/aplicacion/luces/:num_boton/:estado', (req, res) => {
     }
 })
 
+    var resultado;
+    var temperatura;
+    var humedad;
+    var sensacion_termica;
 
+    client.on('message', function(topic, message, packet){
+
+        if(topic === "habitacion/1")
+        {
+            resultado = JSON.parse(message);
+            console.log(resultado);
+            temperatura = resultado['Temperatura'];
+            humedad = resultado['Humedad'];
+            // sensacion_termica = temperatura + 0.348 * (humedad/100 * 6.105 * Math.pow(Math.E,(17.27*temperatura/(237.7+temperatura)))) - 4.25;
+            // console.log(`Sensación térmica: ${sensacion_termica}`);
+        }
+        app.locals.mensajeMQTT = temperatura.toString();
+    })
 
 app.listen(port);
 console.log(`Server on port ${port}`);
