34e347b9fb5f48f4b853bd0bfc15438328f270ea
PabloPaez <pablopaezguerrero07@gmail.com>
2023-06-21 18:11:20 +0200

    no message

--- public/funciones/leerMQTT.js
+++ public/funciones/leerMQTT.js
@@ -11,7 +11,7 @@ module.exports ={
             if(topic === "habitacion/1")
             {
                 resultado = JSON.parse(message);
-                // console.log(resultado);
+                console.log(resultado);
                 temperatura = resultado['Temperatura'];
                 humedad = resultado['Humedad'];
                 // sensacion_termica = temperatura + 0.348 * (humedad/100 * 6.105 * Math.pow(Math.E,(17.27*temperatura/(237.7+temperatura)))) - 4.25;
