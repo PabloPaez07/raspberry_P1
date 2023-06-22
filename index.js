e623d1d49b5ec04db34704017f8be349157636d4
PabloPaez <pablopaezguerrero07@gmail.com>
2023-06-22 17:52:34 +0200

    no message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mqtt = require('mqtt');
 const fs = require('fs');
 const ejs = require('ejs');
 const port = 3000;
@@ -8,6 +9,32 @@ const leerMQTT = require('./public/funciones/leerMQTT');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+const clientId = 'emqx_nodejs_' + Math.random().toString(16).substring(2, 8);
+const username = 'RaspberryPablo';
+const password = 'anv64ahx';
+
+const client = mqtt.connect('ws://broker.emqx.io:8083/mqtt',{
+    clientId,
+    username,
+    password,
+    reconnectPeriod: 1000
+});
+
+client.on('connect', function()
+{
+    console.log('conectado a broker mqtt');
+    client.subscribe('habitacion/1'), function (error)
+    {
+        if(error)
+        {
+            console.log('error conectando a habitacion/1');
+            return;
+        }else{
+            client.publish('habitacion/1',"Hola: soy raspberryPi",0);
+        }
+    }
+});
 
 app.get('/', (req, res) => {
     res.render('pagina_principal.ejs', {
