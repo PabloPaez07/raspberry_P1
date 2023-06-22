66d99bc4c2f9fcc4ec528107f9a22c23bb41ea8f
PabloPaez <pablopaezguerrero07@gmail.com>
2023-06-22 18:18:18 +0200

    no message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mqtt = require('mqtt');
+const GPIO = require('rpi-gpio');
 const fs = require('fs');
 const ejs = require('ejs');
 const port = 3000;
@@ -9,6 +10,13 @@ const leerMQTT = require('./public/funciones/leerMQTT');
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+GPIO.setMode('mode_bcm');
+GPIO.setup(17,'out');
+GPIO.setup(27,'out');
+GPIO.setup(22,'out');
+GPIO.setup(5,'out');
+GPIO.setup(6,'out');
 
 const clientId = 'emqx_nodejs_' + Math.random().toString(16).substring(2, 8);
 const username = 'RaspberryPablo';
@@ -20,7 +28,6 @@ const client = mqtt.connect('ws://broker.emqx.io:8083/mqtt',{
     password,
     reconnectPeriod: 1000
 });
-
 client.on('connect', function()
 {
     console.log('conectado a broker mqtt');
@@ -60,7 +67,31 @@ app.get('/contacto', (req, res) => {
     res.render('contacto.ejs', {
         root:__dirname
     })
-})   
+})
+
+app.get('/aplicacion/luces/:num_habitacion/:estado', (req, res) => {
+    switch(req.params.num_boton)
+    {
+        case "1":
+            GPIO.output(17,(req.params.estado == "1"));
+        break;
+        case "2":
+            GPIO.output(27,(req.params.estado == "1"));
+        break;
+        case "3":
+            GPIO.output(22,(req.params.estado == "1"));
+        break;
+        case "4":
+            GPIO.output(5,(req.params.estado == "1"));
+        break;
+        case "5":
+            GPIO.output(6,(req.params.estado == "1"));
+        break;
+        default:
+            return 0;
+        break;
+    }
+})
 
 app.listen(port);
 console.log(`Server on port ${port}`);
